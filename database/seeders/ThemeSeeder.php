aa8f8365d0ed0ddd7972fc7724f38fe3057b7845
Ankur Singh <abishek.singh.chauhan@gmail.com>
2024-02-06 14:44:22 +0545

    db seeders

--- database/seeders/ThemeSeeder.php
+++ database/seeders/ThemeSeeder.php
@@ -2,10 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use App\Models\Theme;
 
 class ThemeSeeder extends Seeder
 {
