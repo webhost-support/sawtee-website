494f4b17f1a9f35c00c79476f063495640f9c806
Ankur Singh <abishek.singh.chauhan@gmail.com>
2024-04-08 17:31:39 +0545

    Remove unique slug from publication validation.

--- app/Http/Controllers/Admin/PublicationController.php
+++ app/Http/Controllers/Admin/PublicationController.php
@@ -38,7 +38,7 @@ class PublicationController extends Controller
         $validated = $request->validate([
             'category_id' =>'required|numeric|exists:categories,id',
             'title' =>'required|string|max:255',
-            'slug' =>'nullable|string|unique:publications|max:255',
+            'slug' =>'nullable|string|max:255',
             'subtitle' =>'nullable|string|max:255',
             'description' =>'nullable|string|max:2000',
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,webp|max:2048',
