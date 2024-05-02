543ad6e5d8c57e5ba0e50562e5fe87e48bca13e6
Ankur Singh <abishek.singh.chauhan@gmail.com>
2024-05-02 12:22:27 +0545

    Google fonts loading error fix Kantipur fonts loading error fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,17 +3,16 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
         <title inertia>{{ config('app.name', 'SAWTEE') }}</title>
         <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
 
-        {{-- Loads Inter --}}
+        <!-- Loads Inter -->
         @googlefonts
-
         @googlefonts('Roboto')
 
-        {{-- Loads IBM Plex Mono --}}
+        <!-- Loads IBM Plex Mono -->
         @googlefonts('code')
+
         <!-- Load nprogress style -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
         @routes
