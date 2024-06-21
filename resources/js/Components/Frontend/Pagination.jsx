7c06af3b07d9a7785b04907c0397773710e00164
Ankur Singh <abishek.singh.chauhan@gmail.com>
2024-06-21 22:56:58 +0545

    Fix: Default archive overflow in small devices Refactor: Pagination component

--- resources/js/Components/Frontend/Pagination.jsx
+++ resources/js/Components/Frontend/Pagination.jsx
@@ -1,63 +1,38 @@
-import styled from '@emotion/styled';
+import { Box, Stack } from '@chakra-ui/react';
 import { Link } from '@inertiajs/react';
-import { IoIosArrowRoundForward, IoIosArrowRoundBack } from 'react-icons/io';
-import { Box, Stack, Button } from '@chakra-ui/react';
+import { IoIosArrowRoundBack, IoIosArrowRoundForward } from 'react-icons/io';
+import PrimaryButton from '../Backend/PrimaryButton';
+import InertiaChakraLink from './styles/inertia-chakra-link';
 
-export const PaginationButton = styled(Button)`
-	width: 100%;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	font-size: 1rem;
-	// padding: 0.8rem 1rem;
-	// min-height: 50px;
-`;
-
-export const PrevLink = ({ isDisabled, label = 'See older posts', link, ...rest }) => (
-	<Box width="100%" role="group" {...rest}>
-		<StyledLink href={link} aria-disabled={isDisabled}>
-			<PaginationButton colorScheme={'primary'} aria-label={label} isDisabled={isDisabled} variant="outline">
-				<Box
-					className="icon"
-					boxSize="30px"
-					height="auto"
-					as={IoIosArrowRoundBack}
-					_groupHover={{ transform: 'translateX(-10px)' }}
-				/>
-				<span>Newer posts</span>
-			</PaginationButton>
-		</StyledLink>
-	</Box>
-);
-
-export const NextLink = ({ isDisabled, label = 'See newer posts', link, ...rest }) => (
-	<Box width="100%" role="group" {...rest}>
-		<StyledLink href={link} aria-disabled={isDisabled}>
-			<PaginationButton colorScheme={'primary'} aria-label={label} isDisabled={isDisabled} variant="outline">
-				<span>Older posts</span>
-				<Box
-					className="icon"
-					boxSize="30px"
-					height="auto"
-					as={IoIosArrowRoundForward}
-					_groupHover={{ transform: 'translateX(10px)' }}
-				/>
-			</PaginationButton>
-		</StyledLink>
-	</Box>
-);
-
-const Pagination = ({ links, prevPage, nextPage, currentPage, totalPages, ...rest }) => {
-	return (
-		<Stack direction="row" spacing="40px" {...rest}>
-			<PrevLink link={prevPage} isDisabled={!prevPage} />
-			<NextLink link={nextPage} isDisabled={!nextPage} />
-		</Stack>
-	);
+const PaginationButton = ({ link, isDisabled, label = '', icon, slot, children, ...rest }) => {
+  return (
+    <InertiaChakraLink as={Link} href={link} aria-disabled={isDisabled} width="100%" role="group" {...rest}>
+      <PrimaryButton colorScheme={'primary'} aria-label={label} isDisabled={isDisabled} variant="outline" w="full">
+        {slot === 'before' && children}
+        <Box
+          className="icon"
+          boxSize="30px"
+          height="auto"
+          as={icon}
+          _groupHover={{ transform: slot === 'before' ? 'translateX(10px)' : 'translateX(-10px)' }}
+        />
+        {slot === 'after' && children}
+      </PrimaryButton>
+    </InertiaChakraLink>
+  );
 };
 
-const StyledLink = styled(Link)`
-	text-decoration: none;
-`;
+const Pagination = ({ links, prevPage, nextPage, currentPage, totalPages, ...rest }) => {
+  return (
+    <Stack direction="row" spacing="40px" {...rest}>
+      <PaginationButton link={prevPage} isDisabled={!prevPage} slot="after" icon={IoIosArrowRoundBack}>
+        <span>Newer posts</span>
+      </PaginationButton>
+      <PaginationButton link={nextPage} slot="before" isDisabled={!nextPage} icon={IoIosArrowRoundForward}>
+        <span>Older posts</span>
+      </PaginationButton>
+    </Stack>
+  );
+};
 
 export default Pagination;
--- resources/js/Pages/Frontend/Archives/DefaultArchive.jsx
+++ resources/js/Pages/Frontend/Archives/DefaultArchive.jsx
@@ -3,16 +3,23 @@ import { ExploreButton, GlassBox } from '@/Components/Frontend/index';
 import { formatDate } from '@/Utils/helpers';
 import { Box, Heading, LinkBox, LinkOverlay, Stack, Text } from '@chakra-ui/react';
 import { Link } from '@inertiajs/react';
-import React from 'react';
 
-const DefaultArchive = ({ posts, headingColor, textColor, showFallbackImage }) => {
+const DefaultArchive = ({ posts, headingColor, textColor, showFallbackImage, ...rest }) => {
   if (!posts || posts.length <= 0) return 'No posts found';
   return posts.map(post => {
     const featured_image = post.media.filter(media => media.collection_name === 'post-featured-image')[0];
     const file = post.media.filter(media => media.collection_name === 'post-files')[0];
 
     return (
-      <React.Fragment key={post.id}>
+      <GlassBox
+        key={post.id}
+        role="group"
+        w={{ base: 'xs', sm: 'md', md: 'lg', lg: 'xl' }}
+        as={'article'}
+        _hover={{ boxShadow: 'xl' }}
+        pos={'relative'}
+        {...rest}
+      >
         <ArchivePost
           post={post}
           file={file}
@@ -21,7 +28,7 @@ const DefaultArchive = ({ posts, headingColor, textColor, showFallbackImage }) =
           textColor={textColor}
           showFallbackImage={showFallbackImage}
         />
-      </React.Fragment>
+      </GlassBox>
     );
   });
 };
@@ -30,77 +37,69 @@ export default DefaultArchive;
 
 const ArchivePost = ({ post, featured_image, showFallbackImage, file, rest }) => {
   return (
-    <GlassBox
-      role="group"
-      maxW={{ base: '100%', md: 'lg', lg: 'xl', xl: '2xl' }}
-      as={'article'}
-      _hover={{ boxShadow: 'xl' }}
-      {...rest}
-    >
-      <LinkBox>
-        {showFallbackImage && (
-          <PostImageWithOverlay
-            height="240px"
-            borderRadius="var(--chakra-radii-md) var(--chakra-radii-md) 0 0"
-            overflow="hidden"
-            _groupHover={{
-              transition: 'transform 0.4s ease-in-out',
-              borderRadius: 'var(--chakra-radii-md) var(--chakra-radii-md) 0 0',
-              cusrsor: 'pointer',
-            }}
-            src={featured_image && featured_image.original_url}
-            srcSet={
-              featured_image
-                ? ''
-                : `/assets/SM-placeholder.png,
+    <LinkBox {...rest}>
+      {showFallbackImage && (
+        <PostImageWithOverlay
+          height="240px"
+          borderRadius="var(--chakra-radii-md) var(--chakra-radii-md) 0 0"
+          overflow="hidden"
+          _groupHover={{
+            transition: 'transform 0.4s ease-in-out',
+            borderRadius: 'var(--chakra-radii-md) var(--chakra-radii-md) 0 0',
+            cusrsor: 'pointer',
+          }}
+          src={featured_image && featured_image.original_url}
+          srcSet={
+            featured_image
+              ? ''
+              : `/assets/SM-placeholder.png,
                     /assets/SM-placeholder-1024x512.png
                     /assets/SM-placeholder-300x150.png,
                     /assets/SM-placeholder-150x150.png,
                                             `
+          }
+        />
+      )}
+      <Box p={[2, 4]}>
+        <Box>
+          <LinkOverlay
+            as={post.content ? Link : 'a'}
+            target={post.content ? '_self' : '_blank'}
+            href={
+              post.content
+                ? post.category.parent
+                  ? `/category/${post.category.parent.slug}/${post.category.slug}/${post.slug}`
+                  : `/category/${post.category.slug}/${post.slug}`
+                : file?.original_url
             }
-          />
-        )}
-        <Box p={[2, 4]}>
-          <Box>
-            <LinkOverlay
-              as={post.content ? Link : 'a'}
-              target={post.content ? '_self' : '_blank'}
-              href={
-                post.content
-                  ? post.category.parent
-                    ? `/category/${post.category.parent.slug}/${post.category.slug}/${post.slug}`
-                    : `/category/${post.category.slug}/${post.slug}`
-                  : file?.original_url
-              }
-            >
-              <Heading as="h3" fontSize={['sm', 'sm', 'sm', 'md']} fontWeight="normal">
-                {post.title}
-              </Heading>
-            </LinkOverlay>
-            <Text fontSize={'xs'} mt={2} noOfLines={3}>
-              {post.excerpt}
-            </Text>
-          </Box>
-
-          <Box mt="4">
-            <Stack justify="space-between" direction={'row'} flexWrap={'wrap'} alignItems={'center'}>
-              <Text as="time" fontSize={'xs'} color="gray.600">
-                {formatDate(post.published_at)}
-              </Text>
-
-              <ExploreButton
-                size="xs"
-                text="Read more"
-                aria-label={'Read More'}
-                colorScheme={'gray'}
-                w="full"
-                variant={'solid'}
-                link={`/category/${post.category.slug}/${post.slug}`}
-              />
-            </Stack>
-          </Box>
+          >
+            <Heading as="h3" fontSize={['sm', 'sm', 'sm', 'md']} fontWeight="normal">
+              {post.title}
+            </Heading>
+          </LinkOverlay>
+          <Text fontSize={'xs'} mt={2} noOfLines={3}>
+            {post.excerpt}
+          </Text>
         </Box>
-      </LinkBox>
-    </GlassBox>
+
+        <Box mt="4">
+          <Stack justify="space-between" direction={'row'} flexWrap={'wrap'} alignItems={'center'}>
+            <Text as="time" fontSize={'xs'} color="gray.600">
+              {formatDate(post.published_at)}
+            </Text>
+
+            <ExploreButton
+              size="xs"
+              text="Read more"
+              aria-label={'Read More'}
+              colorScheme={'gray'}
+              w="full"
+              variant={'solid'}
+              link={`/category/${post.category.slug}/${post.slug}`}
+            />
+          </Stack>
+        </Box>
+      </Box>
+    </LinkBox>
   );
 };
