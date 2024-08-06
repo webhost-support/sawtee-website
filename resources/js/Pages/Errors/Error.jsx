6c037d51319f78f5e54b52c5e2dac4916463751e
Ankur Singh <abishek.singh.chauhan@gmail.com>
2024-08-06 17:41:43 +0545

    Error page.

--- resources/js/Pages/Errors/Error.jsx
+++ resources/js/Pages/Errors/Error.jsx
@@ -1,5 +1,4 @@
-import Page404 from '@/Components/Frontend/page404';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 import MainLayout from '../../components/Layouts/MainLayout';
 
 const ErrorPage = ({ status, message }) => {
@@ -8,7 +7,23 @@ const ErrorPage = ({ status, message }) => {
       <Head>
         <title>{message}</title>
       </Head>
-      <Page404 status={status} message={message} />
+      <div class="flex h-screen items-center justify-center bg-gray-100">
+        <div class="w-full max-w-md rounded-lg bg-white p-8 shadow-lg">
+          <h1 class="mb-8 text-4xl font-bold text-gray-800">
+            {status} - Page Not Found
+          </h1>
+          <p class="mb-6 text-gray-600">
+            The page you are looking for might have been removed, had its name
+            changed or is temporarily unavailable. {message}
+          </p>
+          <Link
+            href="/"
+            class="inline-block rounded-lg bg-sky-500 px-6 py-3 font-semibold text-white hover:bg-sky-600"
+          >
+            Go back to homepage
+          </Link>
+        </div>
+      </div>
     </MainLayout>
   );
 };
