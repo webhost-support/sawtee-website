8953db80a8ea7b50d2d4a5c10dda10152d0922e8
Ankur Singh <abishek.singh.chauhan@gmail.com>
2024-08-07 16:51:17 +0545

    vite config update.

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: "resources/js/app.jsx",
-            ssr: "resources/js/ssr.jsx",
-            refresh: true,
-        }),
-        react(),
-    ],
-    build: {
-        commonjsOptions: {
-            transformMixedEsModules: true,
-        },
+  plugins: [
+    laravel({
+      input: 'resources/js/app.jsx',
+      ssr: 'resources/js/ssr.jsx',
+      refresh: true,
+    }),
+    react(),
+  ],
+  build: {
+    sourcemap: false,
+    commonjsOptions: {
+      transformMixedEsModules: true,
     },
+  },
 });
