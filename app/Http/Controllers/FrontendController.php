8d8062dd541527c3c9ade6a7e394544a733b0ac0
Ankur Singh <abishek.singh.chauhan@gmail.com>
2025-01-10 13:11:04 +0000

    Slider fix if slider belongs to home page show the slides in the slider in home page.

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -69,7 +69,7 @@ class FrontendController extends Controller
             ->limit(6)
             ->get();
 
-        $slider = Slider::latest()->first();
+        $slider = Slider::where('page_id', Page::where('name', 'home')->first()->id)->latest()->first();
         // dd($slider);
         $slides = $slider ? Slide::where('slider_id', $slider->id)->with('media')->orderBy('id', 'DESC')->take(5)->get() : Array();
         foreach ($slides as $slide) {
